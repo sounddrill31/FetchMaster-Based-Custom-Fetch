93d6a63077c6f42292ce5b997fc6494a70db4842
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2022-04-27 11:06:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 printf "Ensure this is running as sudo"
 mkdir /home/chadfetch
 cd /home/chadfetch
-wget https://raw.githubusercontent.com/anhsirk0/fetch-master-6000/master/fm6000.pl -O fm6000
-wget https://raw.githubusercontent.com/sounddrill31/FetchMaster-Based-Custom-Fetch/main/art.txt -O art.txt
-#change command name if you want to call it something else.
+wget https://raw.githubusercontent.com/anhsirk0/fetch-master-6000/master/fm6000.pl -O /home/chadfetch/fm6000
+wget https://raw.githubusercontent.com/sounddrill31/FetchMaster-Based-Custom-Fetch/main/art.txt -O /home/chadfetch/art.txt
 alias chadfetch="bash /home/chadfetch/fm6000 -f /home/chadfetch/art.txt"
+#change command name if you want to call it something else.
+
